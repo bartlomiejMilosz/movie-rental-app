8dfdab9959d4006fdb355ecd93857a1d89cb5058
Bart Milo <bartlomiej.milosz@gmail.com>
2024-05-01 19:32:31 +0200

    Add customer model

---
+++ src/api/customer/customer.model.js
@@ -0,0 +1,35 @@
+import Joi from "joi";
+import mongoose from "mongoose";
+
+export const Customer = mongoose.model(
+	"Customer",
+	new mongoose.Schema({
+		name: {
+			type: String,
+			required: true,
+			minlength: 5,
+			maxlength: 50,
+		},
+		isGold: {
+			type: Boolean,
+			default: false,
+		},
+		phone: {
+			type: String,
+			required: true,
+			validate: {
+				validator: (v) => {
+					return v.length === 9 && /^\d+$/.test(v);
+				},
+				message: (props) => `${props.value} is not a valid 9-digit phone number!`,
+			},
+		},
+	}),
+);
+
+// Joi schema for Customer validation using the latest version of Joi
+export const customerSchema = Joi.object({
+	name: Joi.string().min(5).required(),
+	isGold: Joi.boolean().required(),
+	phone: Joi.string().length(9).pattern(/^\d+$/).required(),
+});
