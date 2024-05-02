9f51422ebf34b0761a5e0a0e4111ad3ab358ab3b
Bart Milo <bartlomiej.milosz@gmail.com>
2024-05-02 11:56:25 +0200

    Refactor movie router, delete unnecessary comments

--- src/api/movie/movie.router.js
+++ src/api/movie/movie.router.js
@@ -3,22 +3,11 @@ import {deleteMovie, findAll, findById, save, updateMovie, updateMovieGenre} fro
 
 const router = express.Router();
 
-// Get all movies
 router.get("/", findAll);
-
-// Get a single movie by ID
 router.get("/:id", findById);
-
-// Create a new movie
 router.post("/", save);
-
-// Update a movie entirely
 router.put("/:id", updateMovie);
-
-// Update only the genres of a movie
 router.patch("/:id/genres", updateMovieGenre);
-
-// Delete a movie
 router.delete("/:id", deleteMovie);
 
 export { router as movieRouter };
