30905d5613198e72b3115e58031e827832d9176e
Bart Milo <bartlomiej.milosz@gmail.com>
2024-05-02 13:27:57 +0200

    Refactor controller by adding service layer to save method

--- src/api/rental/rental.controller.js
+++ src/api/rental/rental.controller.js
@@ -1,6 +1,5 @@
 import {Rental, validateRental} from "./rental.model.js";
-import {Customer} from "../customer/customer.model.js";
-import {Movie} from "../movie/movie.model.js";
+import rentalService from "./rental.service.js";
 
 export async function findAll(req, res) {
 	try {
@@ -18,43 +17,12 @@ export async function save(req, res) {
 			return res.status(400).send(error.details[0].message);
 		}
 
-		const customer = await Customer.findById(req.body.customerId);
-		if (!customer) {
-			return res.status(400).send("Invalid customer.");
-		}
-
-		const movie = await Movie.findById(req.body.movieId);
-		if (!movie) {
-			return res.status(400).send("Invalid movie.");
-		}
-
-		if (movie.numberInStock === 0) {
-			return res.status(400).send("Movie not in stock.");
-		}
-
-		// Make sure to create a complete movie object
-		let rental = new Rental({
-			customer: {
-				_id: customer._id,
-				name: customer.name,
-				phone: customer.phone,
-				isGold: customer.isGold
-			},
-			movie: {
-				_id: movie._id,
-				title: movie.title,
-				dailyRentalRate: movie.dailyRentalRate,
-				numberInStock: movie.numberInStock,
-				genres: movie.genres
-			},
-			dateOut: req.body.dateOut || new Date(),
-			rentalFee: req.body.rentalFee || 0
-		});
-		rental = await rental.save();
-
-		movie.numberInStock--;
-		await movie.save();  // save the updated movie document
-
+		const rental = await rentalService.createRental(
+			req.body.customerId,
+			req.body.movieId,
+			req.body.dateOut,
+			req.body.rentalFee
+		);
 		res.send(rental);
 	} catch (error) {
 		res.status(500).send(`Failed to save rental: ${error.message}`);
