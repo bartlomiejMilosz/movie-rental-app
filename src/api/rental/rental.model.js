9311289d653b1d543a587d6f256b8cf4eeadf825
Bart Milo <bartlomiej.milosz@gmail.com>
2024-05-02 12:16:40 +0200

    Create better Joi validation

--- src/api/rental/rental.model.js
+++ src/api/rental/rental.model.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import {customerSchema} from "../customer/customer.model.js";
 import {movieSchema} from "../movie/movie.model.js";
 
+//TODO consider reference relation instead of embedded between rental, customer and movie
 const rentalSchema = new mongoose.Schema({
 	customer: {
 		type: customerSchema,
@@ -28,9 +29,19 @@ const rentalSchema = new mongoose.Schema({
 
 export const Rental = mongoose.model("Rental", rentalSchema);
 
+const objectIdValidator = (value, helpers) => {
+	if (!mongoose.Types.ObjectId.isValid(value)) {
+		return helpers.error("any.invalid");
+	}
+	return value;
+};
+
 const rentalSchemaJoi = Joi.object({
-	customerId: Joi.string().required(),
-	movieId: Joi.string().required(),
+	customerId: Joi.string().custom(objectIdValidator, 'Object ID Validation').required(),
+	movieId: Joi.string().custom(objectIdValidator, 'Object ID Validation').required(),
+	dateOut: Joi.date().required(),
+	dateReturned: Joi.date().greater(Joi.ref('dateOut')),
+	rentalFee: Joi.number().min(0)
 });
 
 export function validateRental(rental) {
