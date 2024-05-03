ce45929dbe5b95688fac2daab1b5e3707d0fd892
Bart Milo <bartlomiej.milosz@gmail.com>
2024-05-03 21:14:14 +0200

    Add authentication scope to genre router by specifying user and admin auth middleware

--- src/api/genre/genre.router.js
+++ src/api/genre/genre.router.js
@@ -1,18 +1,15 @@
-import {
-	findAll,
-	findById,
-	create,
-	update,
-	deleteGenre,
-} from "./genre.controller.js";
-import { Router } from "express";
+import {create, deleteGenre, findAll, findById, update,} from "./genre.controller.js";
+import {Router} from "express";
+import {userAuth} from "../../middleware/user.auth.js";
+import {adminAuth} from "../../middleware/admin.auth.js";
 
 const router = Router();
 
 router.get("/", findAll);
 router.get("/:id", findById);
-router.post("/", create);
-router.put("/:id", update);
-router.delete("/:id", deleteGenre);
+// Added userAuth, now can be accessed by setting request header to x-userAuth-token: "token generated during user creation" (or user sign in via user.userAuth.controller)
+router.post("/", userAuth, create);
+router.put("/:id", userAuth, update);
+router.delete("/:id", [userAuth, adminAuth], deleteGenre);
 
 export { router as genreRouter };
