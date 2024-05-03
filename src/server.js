43a1412c9970d486970470cdd77a8005a5521513
Bart Milo <bartlomiej.milosz@gmail.com>
2024-05-03 15:41:07 +0200

    Add config to server

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import mongoose from "mongoose";
+import config from "config";
 import {customerRouter} from "./api/customer/customer.router.js";
 import {genreRouter} from "./api/genre/genre.router.js";
 import {movieRouter} from "./api/movie/movie.router.js";
@@ -7,10 +8,19 @@ import {rentalRouter} from "./api/rental/rental.router.js";
 import {userRouter} from "./api/user/user.router.js";
 import {userAuthRouter} from "./api/user/user.auth.router.js";
 
+if (!config.get("jwtPrivateKey")) {
+	console.error("FATAL ERROR: jwtPrivateKey is not defined!");
+	process.exit(1);
+}
+
 const app = express();
 
+const dbConfig = config.get("db");
+const dbURI = `mongodb://${dbConfig.host}:${dbConfig.port}/${dbConfig.dbName}`;
+
 mongoose
-	.connect("mongodb://localhost:27017/vidly")
+	.connect(dbURI)
+	//.connect("mongodb://localhost:27017/movie-rental")
 	.then(() => console.log("Connected to MongoDB..."))
 	.catch((err) => console.error("Could not connect to MongoDB...", err.message));
 
