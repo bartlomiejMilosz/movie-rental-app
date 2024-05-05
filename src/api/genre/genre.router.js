0a5f91cbfe9564a240dcff3abf230e449e27f74b
Bart Milo <bartlomiej.milosz@gmail.com>
2024-05-05 14:02:15 +0200

    Add created validation middleware to genre router

--- src/api/genre/genre.router.js
+++ src/api/genre/genre.router.js
@@ -1,15 +1,15 @@
-import {create, deleteGenre, findAll, findById, update,} from "./genre.controller.js";
+import {deleteGenre, findAllGenres, findGenreById, saveGenre, updateGenre,} from "./genre.controller.js";
 import {Router} from "express";
-import {userAuth} from "../../middleware/user.auth.js";
-import {adminAuth} from "../../middleware/admin.auth.js";
+import {userAuth} from "../../middleware/auth/user.auth.js";
+import {adminAuth} from "../../middleware/auth/admin.auth.js";
+import {validateGenreMiddleware} from "../../middleware/validation/validate.genre.js";
 
 const router = Router();
 
-router.get("/", findAll);
-router.get("/:id", findById);
-// Added userAuth, now can be accessed by setting request header to x-userAuth-token: "token generated during user creation" (or user sign in via user.userAuth.controller)
-router.post("/", userAuth, create);
-router.put("/:id", userAuth, update);
+router.get("/", findAllGenres);
+router.get("/:id", findGenreById);
+router.post("/", validateGenreMiddleware, userAuth, saveGenre);
+router.put("/:id", validateGenreMiddleware, userAuth, updateGenre);
 router.delete("/:id", [userAuth, adminAuth], deleteGenre);
 
 export { router as genreRouter };
