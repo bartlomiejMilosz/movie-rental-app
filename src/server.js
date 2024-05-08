3bc4a0f8bd0756a0165649dd76d31409e40210f7
Bart Milo <bartlomiej.milosz@gmail.com>
2024-05-08 12:20:19 +0200

    Fix app configuration

--- src/server.js
+++ src/server.js
@@ -18,11 +18,16 @@ const app = express();
 
 const dbConfig = config.get("db");
 const dbURI = `mongodb://${dbConfig.host}:${dbConfig.port}/${dbConfig.dbName}`;
+const dbUriProduction = `mongodb://
+	${config.get("db.username")}:
+	${config.get("db.password",)}@
+	${config.get("db.host")}:
+	${config.get("db.port")}/
+	${config.get("db.dbName")}`;
 
-// const dbUriProduction = `mongodb://${config.get('db.username')}:${config.get('db.password')}@${config.get('db.host')}:${config.get('db.port')}/${config.get('db.dbName')}`;
 mongoose
 	.connect(dbURI)
-	//.connect("mongodb://localhost:27017/movie-rental")
+	//.connect("mongodb://localhost:27017/movie-rental-app")
 	.then(() => console.log("Connected to MongoDB..."))
 	.catch((err) => console.error("Could not connect to MongoDB...", err));
 
